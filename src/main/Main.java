951eb640c4621994a7e2c2ebb7f1af31fc644e94
Christopher Lim <lim.christopher.go@gmail.com>
2020-08-05 16:19:11 +0800

    Create Initial Player Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,27 @@
 package main;
+import resources.players.Player;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of players: ");
+        int nPlayers = scanner.nextInt();
+
+        ArrayList<Player> players = new ArrayList<Player>();
+        String _ = scanner.nextLine();
+        for(int i = 0; i < nPlayers; i++) {
+            String name;
+            System.out.print("Input the name of player " + (i + 1) + ": ");
+            name = scanner.nextLine();
+            players.add(new Player(name));
+        }
+
+        for(Player player : players) {
+            System.out.println(player.getName());
+        }
     }
 }
