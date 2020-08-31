326dfdb7428629c9c0a84bd652cafa9a6a7fa349
Christopher Lim <lim.christopher.go@gmail.com>
2020-09-01 03:40:03 +0800

    Added Test 4 Scripts

--- src/main/tests/Test3.java
+++ src/main/tests/Test3.java
@@ -11,37 +11,33 @@ import main.utilities.InputUtil;
 import java.util.ArrayList;
 
 /**
- * <h1>Test Script #3 (Requirement #2)</h1>
- * <p>This test script is for simulating a user defined number of players to draw the cards from a Deck
- * consisting of Action Cards(Orange Deck) where the first Player to reach a total asset of $40000
- * ([balance - debt] greater than or equal to 40000) wins.</p>
+ * <h1>Test Script #3</h1>
+ * <p>This test script is showing that ActionCard.activate() does the correct action to the
+ * Player just as expected</p>
  *
  * <h1>Sequence:</h1>
  * <ol>
  *     <li>Generate CareerDeck and SalaryDeck</li>
  *     <li>Generate Players and assign the top CareerCard and SalaryCard of both CareerDeck and SalaryDeck</li>
  *     <li>Generate OrangeDeck (composed of ActionCards)</li>
- *     <li>Let Players take turns to draw a Card until a Player reaches the Goal (40000)</li>
+ *     <li>Draw the top card from the Deck</li>
+ *     <li>Activate the Card while comparing the values in the Player</li>
  * </ol>
  *
- * <p>Note: Each turn waits for a Enter key to proceed to the next turn.</p>
- *
- * <p>This shows #2 requirement for the MP Phase 1 submission</p>
+ * <p>This proves that the ActionCard.activate() does the expected action to the player</p>
  *
  * @see Player
  * @see ActionCard
  *
- * @since 08/28/2020
+ * @since 08/30/2020
  */
 public class Test3 {
     public static void main(String[] args) {
-        final int GOAL = 40000;
         Deck careerDeck = Generator.generateCareerDeck();
         Deck salaryDeck = Generator.generateSalaryDeck();
 
-        // asks the user for the number of players (makes sures that the input is only integers {1, 2, 3})
-        int nPlayers = InputUtil.scanInt("Enter number of Players: ", 1, 3);
-        boolean hasWinner = false;
+        // sets 3 as the number of players
+        int nPlayers = 3;
 
         ArrayList<Player> players = new ArrayList<Player>();
         for(int i = 0; i < nPlayers; i++) {
@@ -53,49 +49,27 @@ public class Test3 {
         Deck orangeDeck = Generator.generateOrangeDeck();
         System.out.println(orangeDeck);
         orangeDeck.displayDeck();
-        int turn = 0;
-//        Deck blueDeck = Generator.generateBlueDeck(nPlayers);
-        while(orangeDeck.hasCard() && !hasWinner) {
-            Player currentPlayer = players.get(turn);
 
-            System.out.println("\n" + currentPlayer.getName() + "'s turn: ");
+        System.out.println();
 
-            // picks the top card and assigns it to the currentPlayer
-            ActionCard currentCard = (ActionCard) orangeDeck.pickTopCard();
-            System.out.println(currentPlayer.getName() + " drew -> " + currentCard);
-//            currentCard.displayCard();
+        // draws the top card from the deck
+        ActionCard actionCard = (ActionCard) orangeDeck.pickTopCard();
+        // outputs the ActionCard info
+        System.out.println(actionCard);
 
-            // sets the cards owner and tracks the other players
-            currentCard.setOwner(currentPlayer);
-            currentCard.setOtherPlayers(players.toArray(new Player[0]));
-            // then activates the card
-            currentCard.activate();
+        // set the owner as the player at index 0
+        actionCard.setOwner(players.get(0));
 
-            // show player info and at the same time check if there's a winner
-            System.out.println("Player Stats:");
-            for(Player player : players) {
-                System.out.println("\t" + player);
-                if(player.getBalance() - player.getDebt() >= GOAL) {
-                    hasWinner = true;
-                }
-            }
-            System.out.println(orangeDeck);
-            if(hasWinner) {
-                System.out.println();
-                // if winner was found, display the winner(s)
-                for(Player player : players) {
-                    if(player.getBalance() - player.getDebt() >= GOAL) {
-                        System.out.println(player.getName() + " won! ($" + player.getBalance() + ")");
-                    }
-                }
-            } else {
-                // if not wait for the Player to press the Enter key
-                InputUtil.waitForEnterKey();
+        // set the otherPlayers
+        actionCard.setOtherPlayers(players.toArray(new Player[0]));
 
-                turn++;
-                // reset the turn to 0
-                if(turn == players.size()) turn = 0;
-            }
-        }
+        // output the player before activating the card
+        System.out.println(players.get(0));
+
+        // activate the card
+        actionCard.activate();
+
+        // output the player after activating the card
+        System.out.println(players.get(0));
     }
 }
