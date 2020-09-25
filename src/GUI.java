e690ec3a28ebcd30742625d49c88c9079f229fe9
Christopher Lim <lim.christopher.go@gmail.com>
2020-09-25 11:41:44 +0800

    Style Menu, and Game Settings

--- src/GUI.java
+++ src/GUI.java
@@ -2,6 +2,7 @@ import javafx.fxml.FXMLLoader;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 
